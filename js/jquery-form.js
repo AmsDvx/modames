e1ad322989fc048c336dfec4c59d3694889fecb2
AmsDvx <Amelie@MacBook-Pro-de-Amelie-2.local>
2016-09-01 18:06:08 +0200

    Form questions
    Add new questions

--- js/jquery-form.js
+++ js/jquery-form.js
@@ -1,7 +1,19 @@
+/* testing code*/
 $(function(){
   $("input.qfour").change(function(){
       limit=2;
       nb=$("input.qfour:checked").length;
+      if(nb>limit){
+        $(this).attr("checked", false);
+      }
+  });
+
+});
+/* for two responses code*/
+$(function(){
+  $("input.two-resp").change(function(){
+      limit=2;
+      nb=$("input.two-resp:checked").length;
       if(nb>limit){
         $(this).attr("checked", false);
       }
