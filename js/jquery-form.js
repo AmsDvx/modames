246c2559af9ff10076b32d706efc7bae63b92e25
AmsDvx <Amelie@MacBook-Pro-de-Amelie-2.local>
2016-10-03 16:44:39 +0200

    Form improvment
    - Contents review
    - Colors: put images instead of font awesome
    -  Limit choices for several questions (with jquery)
    - Try to fix pattern check: no success

--- js/jquery-form.js
+++ js/jquery-form.js
@@ -1,8 +1,145 @@
+/* ABOUT DRESSES LENGTH 2 choices maximum*/
+$(function(){
+  $("input.limit-two-dress").change(function(){
+      limit=2;
+      nb=$("input.limit-two-dress:checked").length;
+      if(nb>limit){
+        $(this).attr("checked", false);
+      }
+  });
+
+});
+/* for two responses code*/
+$(function(){
+  $("input.two-resp").change(function(){
+      limit=2;
+      nb=$("input.two-resp:checked").length;
+      if(nb>limit){
+        $(this).attr("checked", false);
+      }
+  });
+
+});
+
+/* ABOUT TOPS LENGTH 2 choices maximum*/
+$(function(){
+  $("input.limit-two-top").change(function(){
+      limit=2;
+      nb=$("input.limit-two-top:checked").length;
+      if(nb>limit){
+        $(this).attr("checked", false);
+      }
+  });
+
+});
+/* for two responses code*/
+$(function(){
+  $("input.two-resp").change(function(){
+      limit=2;
+      nb=$("input.two-resp:checked").length;
+      if(nb>limit){
+        $(this).attr("checked", false);
+      }
+  });
+
+});
+
+/* ABOUT  PANTS STYLE 2 choices maximum*/
+$(function(){
+  $("input.limit-two-pantstyle").change(function(){
+      limit=2;
+      nb=$("input.limit-two-pantstyle:checked").length;
+      if(nb>limit){
+        $(this).attr("checked", false);
+      }
+  });
+
+});
+/* for two responses code*/
+$(function(){
+  $("input.two-resp").change(function(){
+      limit=2;
+      nb=$("input.two-resp:checked").length;
+      if(nb>limit){
+        $(this).attr("checked", false);
+      }
+  });
+
+});
+/* ABOUT JEANS STYLE 2 choices maximum*/
+$(function(){
+  $("input.limit-two-jeanstyle").change(function(){
+      limit=2;
+      nb=$("input.limit-two-jeanstyle:checked").length;
+      if(nb>limit){
+        $(this).attr("checked", false);
+      }
+  });
+
+});
+/* for two responses code*/
+$(function(){
+  $("input.two-resp").change(function(){
+      limit=2;
+      nb=$("input.two-resp:checked").length;
+      if(nb>limit){
+        $(this).attr("checked", false);
+      }
+  });
+
+});
+
+
 /* ABOUT NO MATERIAL ARTICLES 2 choices maximum*/
 $(function(){
-  $("input.limit-two").change(function(){
+  $("input.limit-two-material").change(function(){
       limit=2;
-      nb=$("input.limit-two:checked").length;
+      nb=$("input.limit-two-material:checked").length;
+      if(nb>limit){
+        $(this).attr("checked", false);
+      }
+  });
+
+});
+/* for two responses code*/
+$(function(){
+  $("input.two-resp").change(function(){
+      limit=2;
+      nb=$("input.two-resp:checked").length;
+      if(nb>limit){
+        $(this).attr("checked", false);
+      }
+  });
+
+});
+
+/* ABOUT COLORS 2 choices maximum*/
+$(function(){
+  $("input.limit-two-colors").change(function(){
+      limit=2;
+      nb=$("input.limit-two-colors:checked").length;
+      if(nb>limit){
+        $(this).attr("checked", false);
+      }
+  });
+
+});
+/* for two responses code*/
+$(function(){
+  $("input.two-resp").change(function(){
+      limit=2;
+      nb=$("input.two-resp:checked").length;
+      if(nb>limit){
+        $(this).attr("checked", false);
+      }
+  });
+
+});
+/* ABOUT PATTERN 2 choices maximum*/
+$(function(){
+  $("input.limit-two-pattern").change(function(){
+      limit=2;
+      nb=$("input.limit-two-pattern:checked").length;
       if(nb>limit){
         $(this).attr("checked", false);
       }
